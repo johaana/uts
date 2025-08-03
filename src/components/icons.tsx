426c4178b2511d48f1b34419446be1458d1faf36
Aditi Khan <imiadu@gmail.com>
2025-08-03 20:28:26 +0000

    still not whole logo, please improvise the placement of logo. also use c

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function UtsavsLogo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={cn("flex items-center justify-center", className)} {...props}>
-      <Image src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" alt="Utsavopedia Logo" width={180} height={45} priority />
+      <Image src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" alt="Utsavopedia Logo" width={180} height={45} priority unoptimized />
     </div>
   );
 }
