93745ed41a87f7827d57459af5493a719d94f569
Aditi Khan <imiadu@gmail.com>
2025-08-03 06:21:04 +0000

    still incorrect, you can check names of images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { FestivalCalendar } from "@/components/FestivalCalendar";
 export default function Home() {
   const upcomingFestivals = [
     { name: "Raksha Bandhan", date: "August 19, 2025", link: "/festivals/raksha-bandhan", image: "https://i.postimg.cc/q7qRrp7r/raksha-bandhan.jpg", hint: "rakhi thread", position: "center" },
-    { name: "Ganesh Chaturthi", date: "August 27, 2025", link: "/festivals/ganesh-chaturthi", image: "https://i.postimg.cc/vBZvvbrW/prchi-palwe-Wci-Kb-LIFGxc-unsplash.jpg", hint: "ganesha idol", position: "center" },
+    { name: "Ganesh Chaturthi", date: "August 27, 2025", link: "/festivals/ganesh-chaturthi", image: "https://i.postimg.cc/tTV41DPm/ganpati1.jpg", hint: "ganesha idol", position: "center" },
     { name: "Onam", date: "September 7, 2025", link: "/festivals/onam", image: "https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg", hint: "onam sadhya", position: "center" },
     { name: "Navratri", date: "September 22, 2025", link: "/festivals/navratri", image: "https://i.postimg.cc/0Nffmbwk/sanin-sn-B4-ZQ2m-KEiq-Y-unsplash.jpg", hint: "garba dance", position: "center" },
     { name: "Diwali", date: "October 21, 2025", link: "/festivals/diwali", image: "https://i.postimg.cc/1XNwWtfN/Diwali1.png", hint: "diwali lamps", position: "center" },
