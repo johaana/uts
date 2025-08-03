3fcedb3a9e0a5ff2ef24c88e3a665f8d50a5e114
Aditi Khan <imiadu@gmail.com>
2025-08-03 20:40:14 +0000

    this looks great!! logo crop from up and down and keep central part

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,8 +3,16 @@ import Image from "next/image";
 
 export function UtsavsLogo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className={cn("flex items-center justify-center", className)} {...props}>
-      <Image src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" alt="Utsavopedia Logo" width={180} height={45} priority unoptimized />
+    <div className={cn("relative h-[45px] w-[180px] overflow-hidden", className)} {...props}>
+      <Image 
+        src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" 
+        alt="Utsavopedia Logo" 
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        priority 
+        unoptimized 
+      />
     </div>
   );
 }
