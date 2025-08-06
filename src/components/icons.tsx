e4a4b4b2be3a215ac09d779893b14a83da77769b
Aditi Khan <imiadu@gmail.com>
2025-08-06 03:05:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 
 export function UtsavsLogo() {
   return (
-    <Image 
-      src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" 
-      alt="Utsavs Logo" 
-      layout="fill"
-      objectFit="contain"
-      objectPosition="center"
-      priority 
-      unoptimized
-    />
+    <div className="relative h-20 w-40">
+      <Image 
+        src="https://i.postimg.cc/Gh0W1Kj4/Beige-And-Orange-Traditional-Indian-Fashion-Business-Logo-20250803-235803-0002.png" 
+        alt="Utsavs Logo" 
+        layout="fill"
+        objectFit="contain"
+        objectPosition="center"
+        priority 
+        unoptimized
+      />
+    </div>
   );
 }
