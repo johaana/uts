824db8bb2a07b60e1a80cda52735df113c740b91
Aditi Khan <imiadu@gmail.com>
2025-08-06 02:48:44 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -63,7 +63,7 @@ export function Header() {
       <div className="container mx-auto flex h-20 max-w-7xl items-center justify-between px-4">
         <div className="flex items-center gap-8">
             <Link href="/" className="flex items-center gap-2">
-                <UtsavsLogo className="!h-24 !w-48"/>
+                <UtsavsLogo className="h-24 w-48"/>
             </Link>
         </div>
 
