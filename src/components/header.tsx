1e7bc7ac3aac679a3e773b202b0f005e33b7cc49
Aditi Khan <imiadu@gmail.com>
2025-08-07 20:47:27 +0000

    slightly bigger logo please

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,8 +43,8 @@ export function Header() {
                 <Image 
                   src="https://i.postimg.cc/vZTZ0Br5/utsavs-logo.png" 
                   alt="Utsavs Logo" 
-                  width={isScrolled ? 64 : 72}
-                  height={isScrolled ? 64 : 72}
+                  width={isScrolled ? 72 : 80}
+                  height={isScrolled ? 72 : 80}
                   className="transition-all duration-300"
                 />
              </Link>
