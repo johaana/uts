243a419ea2016e6ff839424ed3e5b7be4978a0e9
Aditi Khan <imiadu@gmail.com>
2025-08-07 05:27:27 +0000

    not yet. please remove 2024 data from upcoming. this is Aug 2025 so anyt

--- src/components/UpcomingFestivalsCarousel.tsx
+++ src/components/UpcomingFestivalsCarousel.tsx
@@ -12,16 +12,16 @@ import {
 import { UpcomingFestivalCard } from "./UpcomingFestivalCard";
 import { parseISO, isFuture, isToday } from 'date-fns';
 
-// This data is now self-contained within the client component.
 const allFestivalsData = [
-  { name: "Raksha Bandhan", date: "2024-08-19T00:00:00", link: "/festivals/raksha-bandhan", image: "https://i.postimg.cc/9MXxXQhY/Raksha-Bandhan.jpg", hint: "rakhi festival" },
-  { name: "Ganesh Chaturthi", date: "2024-09-07T00:00:00", link: "/festivals/ganesh-chaturthi", image: "https://i.postimg.cc/VNWGcb3N/ganesh-chaturthi-festival.jpg", hint: "ganesha idol" },
-  { name: "Onam", date: "2024-09-15T00:00:00", link: "/festivals/onam", image: "https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg", hint: "onam feast" },
-  { name: "Navratri", date: "2024-10-03T00:00:00", link: "/festivals/navratri", image: "https://i.postimg.cc/GhWjwdnN/Navratri.jpg", hint: "garba dance" },
-  { name: "Durga Puja", date: "2024-10-09T00:00:00", link: "/festivals/durga-puja", image: "https://i.postimg.cc/nL3Jwd9d/East-India-festivals.webp", hint: "durga idol" },
-  { name: "Diwali", date: "2024-10-31T00:00:00", link: "/festivals/diwali", image: "https://i.postimg.cc/SjF8HhM1/Diwali2.jpg", hint: "diwali celebration" },
-  { name: "Lohri", date: "2025-01-13T00:00:00", link: "/festivals/lohri", image: "https://i.postimg.cc/kGQ9w7QS/north-india-festivals.webp", hint: "lohri bonfire" },
-  { name: "Holi", date: "2025-03-14T00:00:00", link: "/festivals/holi", image: "https://i.postimg.cc/fWFvx4J9/aceofnet-PNd98-z-An-U0-unsplash.jpg", hint: "holi celebration" }
+  { name: "Onam", date: "2025-09-05T00:00:00", link: "/festivals/onam", image: "https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg", hint: "onam feast" },
+  { name: "Ganesh Chaturthi", date: "2025-08-27T00:00:00", link: "/festivals/ganesh-chaturthi", image: "https://i.postimg.cc/VNWGcb3N/ganesh-chaturthi-festival.jpg", hint: "ganesha idol" },
+  { name: "Navratri", date: "2025-09-22T00:00:00", link: "/festivals/navratri", image: "https://i.postimg.cc/GhWjwdnN/Navratri.jpg", hint: "garba dance" },
+  { name: "Durga Puja", date: "2025-09-27T00:00:00", link: "/festivals/durga-puja", image: "https://i.postimg.cc/nL3Jwd9d/East-India-festivals.webp", hint: "durga idol" },
+  { name: "Diwali", date: "2025-10-20T00:00:00", link: "/festivals/diwali", image: "https://i.postimg.cc/SjF8HhM1/Diwali2.jpg", hint: "diwali celebration" },
+  { name: "Guru Nanak Jayanti", date: "2025-11-05T00:00:00", link: "/festivals/guru-nanak-jayanti", image: "https://i.postimg.cc/zXgfBv19/karah-prasad.jpg", hint: "sikh prayer" },
+  { name: "Christmas", date: "2025-12-25T00:00:00", link: "/festivals/christmas", image: "https://i.postimg.cc/ncKMYdWy/christmas-2.jpg", hint: "christmas decor" },
+  { name: "Lohri", date: "2026-01-13T00:00:00", link: "/festivals/lohri", image: "https://i.postimg.cc/kGQ9w7QS/north-india-festivals.webp", hint: "lohri bonfire" },
+  { name: "Holi", date: "2026-03-04T00:00:00", link: "/festivals/holi", image: "https://i.postimg.cc/fWFvx4J9/aceofnet-PNd98-z-An-U0-unsplash.jpg", hint: "holi celebration" }
 ];
 
 interface Festival {
@@ -44,14 +44,16 @@ export function UpcomingFestivalsCarousel() {
         if (isClient) {
             const now = new Date();
             now.setHours(0, 0, 0, 0); 
-            const filtered = allFestivalsData.filter(festival => {
-                try {
-                    const festivalDate = parseISO(festival.date);
-                    return isFuture(festivalDate) || isToday(festivalDate);
-                } catch (e) {
-                    return false;
-                }
-            });
+            const filtered = allFestivalsData
+                .sort((a, b) => parseISO(a.date).getTime() - parseISO(b.date).getTime())
+                .filter(festival => {
+                    try {
+                        const festivalDate = parseISO(festival.date);
+                        return isFuture(festivalDate) || isToday(festivalDate);
+                    } catch (e) {
+                        return false;
+                    }
+                });
             setUpcomingFestivals(filtered);
         }
     }, [isClient]);
