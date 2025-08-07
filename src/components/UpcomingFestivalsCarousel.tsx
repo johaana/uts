629f8c4072462033da7046bd1d78ebba13195f1f
Aditi Khan <imiadu@gmail.com>
2025-08-07 05:29:38 +0000

    this is perfect. please dont miss raksha bandhan. you are on the right t

--- src/components/UpcomingFestivalsCarousel.tsx
+++ src/components/UpcomingFestivalsCarousel.tsx
@@ -13,10 +13,10 @@ import { UpcomingFestivalCard } from "./UpcomingFestivalCard";
 import { parseISO, isFuture, isToday } from 'date-fns';
 
 const allFestivalsData = [
-  { name: "Onam", date: "2025-09-05T00:00:00", link: "/festivals/onam", image: "https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg", hint: "onam feast" },
+  { name: "Raksha Bandhan", date: "2025-08-09T00:00:00", link: "/festivals/raksha-bandhan", image: "https://i.postimg.cc/k5yL5d42/rakhi.webp", hint: "rakhi thread" },
   { name: "Ganesh Chaturthi", date: "2025-08-27T00:00:00", link: "/festivals/ganesh-chaturthi", image: "https://i.postimg.cc/VNWGcb3N/ganesh-chaturthi-festival.jpg", hint: "ganesha idol" },
+  { name: "Onam", date: "2025-09-05T00:00:00", link: "/festivals/onam", image: "https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg", hint: "onam feast" },
   { name: "Navratri", date: "2025-09-22T00:00:00", link: "/festivals/navratri", image: "https://i.postimg.cc/GhWjwdnN/Navratri.jpg", hint: "garba dance" },
-  { name: "Durga Puja", date: "2025-09-27T00:00:00", link: "/festivals/durga-puja", image: "https://i.postimg.cc/nL3Jwd9d/East-India-festivals.webp", hint: "durga idol" },
   { name: "Diwali", date: "2025-10-20T00:00:00", link: "/festivals/diwali", image: "https://i.postimg.cc/SjF8HhM1/Diwali2.jpg", hint: "diwali celebration" },
   { name: "Guru Nanak Jayanti", date: "2025-11-05T00:00:00", link: "/festivals/guru-nanak-jayanti", image: "https://i.postimg.cc/zXgfBv19/karah-prasad.jpg", hint: "sikh prayer" },
   { name: "Christmas", date: "2025-12-25T00:00:00", link: "/festivals/christmas", image: "https://i.postimg.cc/ncKMYdWy/christmas-2.jpg", hint: "christmas decor" },
