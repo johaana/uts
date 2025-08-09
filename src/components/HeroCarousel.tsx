ac2f9ac2fd5d3f8f11281e27b23e3d9ec1a32e52
Aditi Khan <imiadu@gmail.com>
2025-08-09 12:39:25 +0000

    remove navratri, south cover one, replace with others

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -7,9 +7,9 @@ import { cn } from '@/lib/utils';
 
 const images = [
   { src: 'https://i.postimg.cc/rmVJnj2w/Pushkar-Camel-Fair.avif', alt: 'Pushkar Camel Fair', hint: 'pushkar camel fair' },
-  { src: 'https://i.postimg.cc/j5CbVzzR/south-indian-festivals.jpg', alt: 'Thrissur Pooram', hint: 'kerala temple festival' },
+  { src: 'https://i.postimg.cc/4xZYzGsQ/Goa-Carnival.jpg', alt: 'Goa Carnival', hint: 'carnival parade float' },
   { src: 'https://i.postimg.cc/nL3Jwd9d/East-India-festivals.webp', alt: 'Durga Puja', hint: 'durga idol' },
-  { src: 'https://i.postimg.cc/J4JFtVYT/navratri1.jpg', alt: 'Navratri Garba Dance', hint: 'garba dance' },
+  { src: 'https://i.postimg.cc/0564g0S7/nandu-menon-h-GHldb-Cg-YDA-unsplash.jpg', alt: 'Onam Sadya Feast', hint: 'onam feast' },
   { src: 'https://i.postimg.cc/6pL9wGsd/Hornbill-Festival.webp', alt: 'Hornbill Festival', hint: 'naga festival' },
 ];
 
