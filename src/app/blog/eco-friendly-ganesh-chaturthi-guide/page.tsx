c0536a7284ad359deea85307adc62b0b1935d926
Aditi Khan <imiadu@gmail.com>
2025-08-17 18:19:08 +0000

    check this. this blog has no image. you may use - <a href='https://posti

--- src/app/blog/eco-friendly-ganesh-chaturthi-guide/page.tsx
+++ src/app/blog/eco-friendly-ganesh-chaturthi-guide/page.tsx
@@ -12,7 +12,7 @@ import { products } from "@/lib/product-data";
 
 const post = { 
     title: "Your Ultimate Guide to an Eco-Friendly Ganesh Chaturthi", 
-    image: "https://i.postimg.cc/PqjW2Gmm/eco-friendly-ganesha.jpg",
+    image: "https://i.postimg.cc/nr024HN7/eco-ganesh-3.jpg",
     excerpt: "Celebrate the birth of Lord Ganesha with reverence for both tradition and nature. Discover beautiful, water-soluble idols, DIY clay kits, and sustainable decoration ideas for a joyous and conscious festival."
 };
 
