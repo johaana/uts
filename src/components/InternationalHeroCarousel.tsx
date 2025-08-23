0f75ca59eb1ad129898d82e45f1570c3f248e020
Aditi Khan <imiadu@gmail.com>
2025-08-23 01:00:31 +0000

    remove some - just keep or only use any or all of these images - la toma

--- src/components/InternationalHeroCarousel.tsx
+++ src/components/InternationalHeroCarousel.tsx
@@ -8,15 +8,19 @@ import { internationalEvents } from '@/lib/festival-data';
 
 // A curated list of visually appealing and diverse festivals for the carousel
 const carouselImages = [
-  { src: 'https://i.postimg.cc/XJs7rMdt/Yi-Peng-Lantern-Festival-Thailand.webp', alt: 'Yi Peng Lantern Festival, Thailand', hint: 'sky lanterns' },
-  { src: 'https://i.postimg.cc/QCVN56qC/Venice-Carnival-Italy.jpg', alt: 'Venice Carnival, Italy', hint: 'venetian masks' },
-  { src: 'https://i.postimg.cc/RhHJcbgG/songkran.avif', alt: 'Songkran Water Festival, Thailand', hint: 'water festival' },
   { src: 'https://i.postimg.cc/GhwH7wCz/la-tomatina1.jpg', alt: 'La Tomatina, Spain', hint: 'tomato fight' },
   { src: 'https://i.postimg.cc/0r1MZd3v/Carnival.jpg', alt: 'Rio Carnival, Brazil', hint: 'brazil carnival' },
-  { src: 'https://i.postimg.cc/g0q0PLJz/Oktoberfest-Munich.jpg', alt: 'Oktoberfest, Germany', hint: 'beer festival' },
-  { src: 'https://i.postimg.cc/RCcT1Twd/kukur-tihar1.avif', alt: 'Kukur Tihar, Nepal', hint: 'dog worship' },
   { src: 'https://i.postimg.cc/fLzRWWtx/Hogmanay-Scotland.webp', alt: 'Hogmanay, Scotland', hint: 'torchlight procession' },
-  { src: 'https://i.postimg.cc/cJbJfPhR/thaipusam.webp', alt: 'Thaipusam', hint: 'kavadi' },
+  { src: 'https://i.postimg.cc/1XTmKpWH/St-Patrick-s-Day-Ireland-Global.avif', alt: 'St. Patrick\'s Day', hint: 'irish parade' },
+  { src: 'https://i.postimg.cc/QCVN56qC/Venice-Carnival-Italy.jpg', alt: 'Venice Carnival, Italy', hint: 'venetian masks' },
+  { src: 'https://i.postimg.cc/Xvs535ZK/Harbin-Ice-and-Snow-Festival.webp', alt: 'Harbin Ice and Snow Festival, China', hint: 'ice sculptures' },
+  { src: 'https://i.postimg.cc/g0q0PLJz/Oktoberfest-Munich.jpg', alt: 'Oktoberfest, Germany', hint: 'beer festival' },
+  { src: 'https://i.postimg.cc/htb3BK98/cherry-blossom-festival.jpg', alt: 'Cherry Blossom Festival (Hanami), Japan', hint: 'cherry blossoms' },
+  { src: 'https://i.postimg.cc/05F4GWkS/Dia-de-los-Muertos-Day-of-the-Dead-3.avif', alt: 'Día de los Muertos, Mexico', hint: 'day of the dead' },
+  { src: 'https://i.postimg.cc/Y2yDxX37/Inti-Raymi-Festival-of-the-Sun.jpg', alt: 'Inti Raymi, Peru', hint: 'inca festival' },
+  { src: 'https://i.postimg.cc/TYb4tV14/chinese-new-year.webp', alt: 'Chinese New Year', hint: 'dragon dance' },
+  { src: 'https://i.postimg.cc/5yrkPw0H/Krampusnacht.avif', alt: 'Krampusnacht, Austria', hint: 'krampus' },
+  { src: 'https://i.postimg.cc/RCcT1Twd/kukur-tihar1.avif', alt: 'Kukur Tihar, Nepal', hint: 'dog worship' },
 ];
 
 export function InternationalHeroCarousel() {
