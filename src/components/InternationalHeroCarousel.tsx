789fcd2bb6f58fbc61b212f0a9b6449a3ab7f39c
Aditi Khan <imiadu@gmail.com>
2025-08-23 00:54:01 +0000

    try to fit in whole images in international festivals, please use - la t

--- src/components/InternationalHeroCarousel.tsx
+++ src/components/InternationalHeroCarousel.tsx
@@ -11,9 +11,10 @@ const carouselImages = [
   { src: 'https://i.postimg.cc/XJs7rMdt/Yi-Peng-Lantern-Festival-Thailand.webp', alt: 'Yi Peng Lantern Festival, Thailand', hint: 'sky lanterns' },
   { src: 'https://i.postimg.cc/QCVN56qC/Venice-Carnival-Italy.jpg', alt: 'Venice Carnival, Italy', hint: 'venetian masks' },
   { src: 'https://i.postimg.cc/RhHJcbgG/songkran.avif', alt: 'Songkran Water Festival, Thailand', hint: 'water festival' },
-  { src: 'https://i.postimg.cc/htb3BK98/cherry-blossom-festival.jpg', alt: 'Cherry Blossom Festival, Japan', hint: 'cherry blossoms' },
+  { src: 'https://i.postimg.cc/GhwH7wCz/la-tomatina1.jpg', alt: 'La Tomatina, Spain', hint: 'tomato fight' },
+  { src: 'https://i.postimg.cc/0r1MZd3v/Carnival.jpg', alt: 'Rio Carnival, Brazil', hint: 'brazil carnival' },
   { src: 'https://i.postimg.cc/g0q0PLJz/Oktoberfest-Munich.jpg', alt: 'Oktoberfest, Germany', hint: 'beer festival' },
-  { src: 'https://i.postimg.cc/05F4GWkS/Dia-de-los-Muertos-Day-of-the-Dead-3.avif', alt: 'Día de los Muertos, Mexico', hint: 'day of the dead' },
+  { src: 'https://i.postimg.cc/RCcT1Twd/kukur-tihar1.avif', alt: 'Kukur Tihar, Nepal', hint: 'dog worship' },
   { src: 'https://i.postimg.cc/fLzRWWtx/Hogmanay-Scotland.webp', alt: 'Hogmanay, Scotland', hint: 'torchlight procession' },
   { src: 'https://i.postimg.cc/cJbJfPhR/thaipusam.webp', alt: 'Thaipusam', hint: 'kavadi' },
 ];
